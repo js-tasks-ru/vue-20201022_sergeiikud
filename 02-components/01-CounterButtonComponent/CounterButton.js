9af574979346aed99ef979ebb14f3ab70df5c8ad
Sergii Kudriashov <skudriashov@yaypay.com>
2020-10-27 02:04:08 +0200

    add solution for 02-components 03-counter-button-component

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,10 +1,35 @@
 export const CounterButton = {
-  // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
-
-  // Компонент должен иметь входной параметр
-
-  // Компонент должен иметь модель
-
-  // Шаблон лучше держать максимально простым, а логику выносить в методы
+  template: '<button type="button" @click="increment">{{ counter }}</button>',
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    },
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    counter: {
+      get() {
+        return this.count || this.value;
+      },
+      set(val) {
+        this.$emit('increment', val);
+        this.$emit('input', val);
+      }
+    }
+  },
+  created() {
+    this.setMissingValue();
+  },
+  methods: {
+    setMissingValue() {
+      if (!this.count && !this.value) this.counter = 0;
+    },
+    increment() {
+      this.counter += 1;
+    }
+  }
 };
